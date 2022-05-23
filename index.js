a500542247013754a19bd99b59eb2723bc35bd13
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2022-05-23 20:41:18 -0300

    Tratamento de erros

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'O CAMINHO ESTÁ ERRADO!'))
+}
+
 function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8'
-  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
-    console.log(chalk.green(texto))
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+    if (erro) {
+      trataErro(erro);
+    }
+    console.log(chalk.green(texto));
   })
 }
 
