e7a446d7ab52b38ce93dfb511c00bbd221cbb7f1
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2022-05-23 21:21:14 -0300

    async/await

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const chalk = require('chalk');
 const fs = require('fs');
 
 function trataErro(erro) {
-  throw new Error(chalk.red(erro.code, 'O CAMINHO ESTÁ ERRADO!'))
+  throw new Error(chalk.red(erro.code, 'O CAMINHO ESTÁ ERRADO!'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-  const encoding = 'utf-8'
-  fs.promises
-  .readFile(caminhoDoArquivo, encoding)
-  .then((texto) => console.log(chalk.green(texto)))
-  .catch((erro) => trataErro(erro))
+async function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto))
+  } catch(erro) {
+    trataErro(erro);
+  }
 }
 
 pegaArquivo('./arquivos/texto1.md')
