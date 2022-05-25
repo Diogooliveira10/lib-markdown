14ee77369c803a321ebede16d865e3a2ed4b3146
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2022-05-25 20:41:28 -0300

    Capturando grupos

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento, a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégios podem criar objetos File representando qualquer arquivo local sem a interação do usuário (veja [Implementation](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) notes para mais informações).'
+
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const linksExtraidos = texto.match(regex);
+  console.log(linksExtraidos);
+}
+
+extraiLinks(texto);
+
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'O CAMINHO ESTÁ ERRADO!'));
 }
@@ -15,4 +25,4 @@ async function pegaArquivo(caminhoDoArquivo) {
   }
 }
 
-pegaArquivo('./arquivos/texto1.md')
+// pegaArquivo('./arquivos/texto1.md')
