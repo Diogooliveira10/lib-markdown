2bef449b326d256fe53fcf225e88bd2fb03f1406
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2022-05-25 21:22:54 -0300

    Retornando links

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento, a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégios podem criar objetos File representando qualquer arquivo local sem a interação do usuário (veja [Implementation](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) notes para mais informações).'
-
 function extraiLinks(texto) {
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-  const linksExtraidos = texto.match(regex);
-  console.log(linksExtraidos);
+  const arrayResultados = [];
+  let temp;
+  while((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] })
+  }
+  return arrayResultados;
 }
-
-extraiLinks(texto);
 
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'O CAMINHO ESTÁ ERRADO!'));
@@ -19,10 +19,10 @@ async function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto))
+    console.log(extraiLinks(texto));
   } catch(erro) {
     trataErro(erro);
   }
 }
 
-// pegaArquivo('./arquivos/texto1.md')
+pegaArquivo('./arquivos/texto1.md');
