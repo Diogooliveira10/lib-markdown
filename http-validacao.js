148798520aa9fc9a29328674cf79f767fe46604a
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2022-06-22 20:22:30 -0300

    Montando o objeto

--- http-validacao.js
+++ http-validacao.js
@@ -1,9 +1,41 @@
-function geraArrayDeURLs(arrayLinks) {
-    return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+const fetch = (...args) => import('node-fetch')
+    .then(({default: fetch}) => fetch(...args));
+
+function manejaErros(erro) {
+    throw new Error(erro.message);
 }
 
-function validaURLs(arrayLinks) {
-    return geraArrayDeURLs(arrayLinks);
+async function checaStatus(arrayURLs) {
+    try {
+        const arrayStatus = await Promise
+            .all(arrayURLs
+                .map(async url => {
+                const res = await fetch(url)
+                return res.status;
+        }))
+        return arrayStatus;
+
+    } catch(erro) {
+        manejaErros(erro);
+    }
+}
+
+function geraArrayDeURLs(arrayLinks) {
+    return arrayLinks
+        .map(objetoLink => Object
+            .values(objetoLink).join());
+}
+
+async function validaURLs(arrayLinks) {
+    const links = geraArrayDeURLs(arrayLinks);
+    const statusLinks = await checaStatus(links);
+    
+    const resultados = arrayLinks.map((objeto, indice) => ({
+        ...objeto, 
+        status: statusLinks[indice]
+    }))
+    
+    return resultados;
 }
 
 module.exports = validaURLs;
