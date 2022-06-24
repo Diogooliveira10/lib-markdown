3b557391f74683a06c04dc7d33843844e9536bd8
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2022-06-23 22:10:40 -0300

    Adicionando testes

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,18 @@
 const pegaArquivo = require('../index');
 
-test('deve ser um função', () => {
+const arrayResult = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
+]
+
+describe('pegaArquivo::', () => {
+  it('deve ser uma função', () => {
     expect(typeof pegaArquivo).toBe('function');
-  });
+  })
+  
+  it('deve retornar array com resultados', async () => {
+    const resultado = await pegaArquivo('') //CAMINHO DO ARQUIVO 
+    expect(resultado).toEqual(arrayResult)
+  })
+});
